ebbc2c77b877a0f61edbe95fab6be75b6e31be50
Artur Krivorokov <artur.krivorokov@gmail.com>
2021-03-21 21:43:53 +0200

    Renamed properly final static variables. Redone compareLastEightBits() and countMatches() matches, so that they do what is says - one compares and other counts.

--- Generator.java
+++ Generator.java
@@ -1,29 +1,30 @@
 package com.company;
 
 public class Generator {
-    final int factorA = 16807;
-    final int factorB = 48271;
-    final int dividedBy = 2147483647;
-    static int counter = 0;
+    final static long FACTOR_A = 16807;
+    final static long FACTOR_B = 48271;
+    final static long DIVIDED_BY = 2147483647;
 
     public long generateNumber(long number, long factor) {
-        return Long.remainderUnsigned((number*factor),dividedBy);
+        return (number * factor) % DIVIDED_BY;
     }
 
-    public void compareLastEightBits(long num1, long num2) {
-        if (num1 % 256 == num2 % 256)
+    public boolean compareLastEightBits(long num1, long num2) {
+        return  num1 % 256 == num2 % 256;
+    }
+
+    public int countMatches(long num1, long num2) {
+        int counter = 0;
+        long generatedNumber1 = generateNumber(num1, FACTOR_A);
+        long generatedNumber2 = generateNumber(num2, FACTOR_B);
+        if (compareLastEightBits(generatedNumber1, generatedNumber2))
             counter++;
-    }
-
-    public void countMatches(long num1, long num2) {
-        long generatedNumber1 = generateNumber(num1, factorA);
-        long generatedNumber2 = generateNumber(num2, factorB);
-        compareLastEightBits(generatedNumber1, generatedNumber2);
         for (int i = 0; i < 999999; i++) {
-            generatedNumber1 = generateNumber(generatedNumber1, factorA);
-            generatedNumber2 = generateNumber(generatedNumber2, factorB);
-            compareLastEightBits(generatedNumber1, generatedNumber2);
+            generatedNumber1 = generateNumber(generatedNumber1, FACTOR_A);
+            generatedNumber2 = generateNumber(generatedNumber2, FACTOR_B);
+            if (compareLastEightBits(generatedNumber1, generatedNumber2))
+                counter++;
         }
-        System.out.println(counter);
+        return counter;
     }
 }
--- Main.java
+++ Main.java
@@ -4,13 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
         if (args.length == 2) {
-            int initialNumberA = Integer.parseInt(args[0]);
-            int initialNumberB = Integer.parseInt(args[1]);
+            long initialNumberA = Integer.parseInt(args[0]);
+            long initialNumberB = Integer.parseInt(args[1]);
             Generator generator = new Generator();
-            generator.countMatches(initialNumberA, initialNumberB);
+            System.out.println(generator.countMatches(initialNumberA, initialNumberB));
             } else {
             System.out.println("Please provide 2 integer parameters to the application");
         }
-
     }
 }
